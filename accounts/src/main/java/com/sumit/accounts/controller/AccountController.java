f81d72171e09b3ac5dcc9572c4ec84c8ba8003ba
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-12-11 21:05:33 -0500

    Retry at individual MS level

--- accounts/src/main/java/com/sumit/accounts/controller/AccountController.java
+++ accounts/src/main/java/com/sumit/accounts/controller/AccountController.java
@@ -4,8 +4,11 @@ import com.sumit.accounts.dto.ContactInfoDto;
 import com.sumit.accounts.dto.CustomerDto;
 import com.sumit.accounts.dto.ResponseDto;
 import com.sumit.accounts.service.AccountService;
+import io.github.resilience4j.retry.annotation.Retry;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -18,6 +21,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 @Validated
 public class AccountController {
+
+    private static final Logger log = LoggerFactory.getLogger(AccountController.class);
 
     @Autowired
     private AccountService accountService;
@@ -64,16 +69,26 @@ public class AccountController {
                                  .body(new ResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()));
     }
 
-    @GetMapping("/build-info")
-    public ResponseEntity<String> getBuildInfo() {
-        return ResponseEntity.status(HttpStatus.OK)
-                             .body(buildVersion);
-    }
-
     @GetMapping("/contact-info")
     public ResponseEntity<ContactInfoDto> getContactInfo() {
         return ResponseEntity.status(HttpStatus.OK)
                              .body(contactInfoDto);
+    }
+
+    // 2. Resilience4j retry with fallback mechanism
+    @Retry(name = "getBuildInfo",fallbackMethod = "getBuildInfoFallback")
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo() {
+        log.debug("getBuildInfo() method Invoked");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(buildVersion);
+    }
+
+    public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
+        log.debug("getBuildInfoFallback() method Invoked");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("0.9");
     }
 
 
